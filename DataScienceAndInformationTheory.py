8df903527a865c919135439930df8375e8e72812
Naman Kanwar <nkanwar1@student.gsu.edu>
2018-03-14 02:10:21 -0400

    Final Version
    Updated with comments

--- DataScienceAndInformationTheory.py
+++ DataScienceAndInformationTheory.py
@@ -22,40 +22,50 @@ def letterCount():
     #Return dictionary of characters and total characters
     return dictionaryChar, totalChars
 
+
+#Entropy= Sum(p(xi)*(logBASE 2(1/ p(xi)))
 def entropy(X, total):
     summation = 0.0
     #For every key in dictionary
     for x in X:
-        
+        #p(x)= occurance of x / sample size
+        #Storing (logBASE 2(1/ p(xi))
         logArg= math.log((1/(X[x]/total)), 2)
+        #Adding all entropy values of sample
         summation+= (X[x]/total)*logArg
+    #Return the sample entropy
     return summation
 
-#
-# n= {"0": 0.25, "1": 0.75}
-# print(entropy(n,1))
 
 from PIL import Image
 
 def pixelIntensity():
+    #Open the image and convert it to greyscale
     img = Image.open('image.png').convert('LA')
+    #Save the image as greyscale.png
     img.save('greyscale.png')
+    #Open the greyscale image to read
     img = Image.open('greyscale.png','r')
+    #Storing size of image
     x,y = img.size
+    #Storing image as a list of pixel intensities
     image= list(img.getdata())
     intensity={}
+    #Converting list image into dictionary of intensities
     for i in image:
         if i[0] in intensity:
             intensity[i[0]]+= 1
         else:
             intensity[i[0]] = 1
+    # Return dictionary of intensities and total pixels
     return intensity, x*y
 
 def main():
     dictionaryBook, total=letterCount()
+    #Calculate entropy of book
     print("Book Entropy: ",entropy(dictionaryBook,total)*total)
     dictionaryImage, total=pixelIntensity()
+    #Calculate entropy of image
     print("Image Entropy: ", entropy(dictionaryImage,total)*total)
-
 
 main()
